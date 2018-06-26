9ba710ec743900f74dbde7f3c532755465f7740b
elorsborn <elorsborn17@gmail.com>
2018-06-26 10:03:59 +0100

    Making a simple read message page where the message will be displayed

--- geomessenger/Components/Nav.js
+++ geomessenger/Components/Nav.js
@@ -49,7 +49,7 @@ const styles = StyleSheet.create({
     width: "100%",
     height: 50,
     backgroundColor: "purple",
-    justifyContent: "center",
+    justifyContent: "space-evenly",
     alignItems: "center",
     position: "absolute",
     flexDirection: "row",
