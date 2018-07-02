6e99651d2b4e7e526a63200ab17c30ed9c943ba7
Seth Bell <seth@Seths-MacBook-Pro.local>
2018-07-02 16:18:11 +0100

    Login screen with auth added

--- geomessenger/App.js
+++ geomessenger/App.js
@@ -1,17 +1,16 @@
-import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
-import { createStackNavigator } from 'react-navigation';
-import FriendsScreen from './Components/FriendsScreen';
-import ReadScreen from './Components/ReadScreen';
-import PostScreen from './Components/PostScreen';
-import MapScreen from './Components/MapScreen';
-import HomeScreen from './Components/HomeScreen';
-import LoginScreen from './Components/LoginScreen';
+import React from "react";
+import { StyleSheet, Text, View, Image } from "react-native";
+import { createStackNavigator } from "react-navigation";
+import FriendsScreen from "./Components/FriendsScreen";
+import ReadScreen from "./Components/ReadScreen";
+import PostScreen from "./Components/PostScreen";
+import MapScreen from "./Components/MapScreen";
+import HomeScreen from "./Components/HomeScreen";
+import LoginScreen from "./Components/LoginScreen";
 
 const NavigationApp = createStackNavigator({
   Login: { screen: LoginScreen },
   Home: { screen: HomeScreen },
-  Friends: { screen: FriendsScreen },
   Read: { screen: ReadScreen },
   Post: { screen: PostScreen },
   Map: { screen: MapScreen }
--- geomessenger/Components/LoginScreen.js
+++ geomessenger/Components/LoginScreen.js
@@ -1,7 +1,120 @@
-import Login from 'react-native-simple-login';
+import React from "react";
+import Amplify, { Auth } from "aws-amplify";
+import configAWS from "../configAWS";
+import {
+  Button,
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  KeyboardAvoidingView,
+  Image
+} from "react-native";
 
-const LoginScreen = () => {
-  <Login />;
-};
+import { Icon } from "react-native-elements";
+import * as api from "../api";
+import ModalDropdown from "react-native-modal-dropdown";
+
+class LoginScreen extends React.Component {
+  state = {
+    email: "",
+    password: ""
+  };
+
+  componentDidMount() {
+    Amplify.configure({
+      Auth: {
+        mandatorySignIn: true,
+        region: configAWS.cognito.REGION,
+        userPoolId: configAWS.cognito.USER_POOL_ID,
+        identityPoolId: configAWS.cognito.IDENTITY_POOL_ID,
+        userPoolWebClientId: configAWS.cognito.APP_CLIENT_ID
+      }
+    });
+  }
+  render() {
+    return (
+      <KeyboardAvoidingView
+        style={styles.postContainer}
+        behavior="padding"
+        keyboardShouldPersistTaps={"never"}
+      >
+        <View style={styles.dropdownContainer} />
+        <View style={styles.textInputContainer}>
+          <Icon
+            size={50}
+            color="whitesmoke"
+            name="plus"
+            type="evilicon"
+            onPress={this.handleSubmit}
+          />
+          <TextInput
+            maxLength={140}
+            style={styles.textInput}
+            onChangeText={email => this.setState({ email })}
+            value={this.state.email}
+            placeholder="EMAIL"
+          />
+          <TextInput
+            maxLength={140}
+            style={styles.textInput}
+            onChangeText={password => this.setState({ password })}
+            value={this.state.password}
+            placeholder="PASSWORD"
+          />
+        </View>
+        <Image style={styles.image} source={require("../data/elk.png")} />
+      </KeyboardAvoidingView>
+    );
+  }
+
+  handleSubmit = async event => {
+    event.preventDefault();
+
+    try {
+      await Auth.signIn(this.state.email, this.state.password);
+      alert("Logged in");
+    } catch (e) {
+      alert("Details incorrect - please try again");
+    }
+  };
+}
+
+const styles = StyleSheet.create({
+  image: {
+    height: "20%",
+    width: "30%",
+    marginRight: "auto",
+    marginLeft: "auto"
+  },
+  postContainer: {
+    flex: 1,
+    backgroundColor: "rgb(137, 87, 188)"
+  },
+  textInputContainer: {
+    flex: 1,
+    alignItems: "center",
+    flexDirection: "column",
+    justifyContent: "flex-start"
+  },
+  dropdownContainer: {
+    marginBottom: 0,
+    flex: 1,
+    flexDirection: "column",
+    justifyContent: "flex-end"
+  },
+  textInput: {
+    color: "purple",
+    backgroundColor: "whitesmoke",
+    padding: "2%",
+    width: "80%",
+    height: 100,
+    fontSize: 20,
+    borderColor: "white",
+    borderWidth: 1,
+    borderRadius: 10,
+    marginBottom: 10
+  }
+});
 
 export default LoginScreen;
